d739a54c5c5d66232d5ee8ac6dbc3790dc82ebf5
FaniryKeziah <hei.keziah.2@gmail.com>
2023-08-19 12:36:17 +0300

    feat: add cnaps repository

---
+++ src/main/java/com/example/prog4/repository/cnaps/CnapsRepository.java
@@ -0,0 +1,7 @@
+package com.example.prog4.repository.cnaps;
+
+import com.example.prog4.model.cnaps.Cnaps;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CnapsRepository extends JpaRepository<Cnaps , String> {
+}

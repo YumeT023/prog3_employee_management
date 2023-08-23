625ec7e56bdc714792232dd76f5c928e42c8d62f
FaniryKeziah <hei.keziah.2@gmail.com>
2023-08-23 08:37:55 +0300

    fix: make return findCnapsNumberByEndToEndId

--- src/main/java/com/example/prog4/repository/jpa/cnaps/CnapsEmployeeJpaRepository.java
+++ src/main/java/com/example/prog4/repository/jpa/cnaps/CnapsEmployeeJpaRepository.java
@@ -4,7 +4,9 @@ import com.example.prog4.model.core.entity.cnaps.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface CnapsEmployeeJpaRepository extends JpaRepository<Employee, String> {
     @Query(value = "SELECT * FROM employee WHERE end_to_end_id = :id")
-    String findCnapsNumberByEmployeeId(String id);
+    Optional<String> findCnapsNumberByEndToEndId(String id);
 }
